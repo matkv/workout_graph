1ebe33618304a91ad9afda458dca035bc5302cef
matkv <mail@matkv.dev>
2023-03-07 21:00:43 +0100

    started playing around with deserializing a csv file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::{error::Error, io, process}
+//use serde::{Deserialize}
+
+#[derive(Debug, serde::Deserialize)]
+struct Activity {
+    title: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    read_csv();
+}
+
+fn read_csv() -> Result<(), Box<dyn Error>> {
+    let mut activities = csv::Reader::from_path("activities.csv");
+
+    for activity in activities.unwrap().deserialize() {
+        let workout: Activity = activity?;
+        println!("{:?}", workout);
+    }
+
+    Ok(())
 }
